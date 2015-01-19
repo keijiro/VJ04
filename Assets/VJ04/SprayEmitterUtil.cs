983162aade50f39c10003d9daa0078d5959f205f
Keijiro Takahashi <keijiro@X1-Turbo.local>
2015-01-19 17:10:00 +0900

    Added ASCII Fx and petal effects.

---
+++ Assets/VJ04/SprayEmitterUtil.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SprayEmitterUtil : MonoBehaviour
+{
+    public Transform snapTo;
+
+    void Update()
+    {
+        var spray = GetComponent<Kvant.Spray>();
+
+        if (snapTo) spray.emitterPosition = snapTo.position;
+    }
+}
