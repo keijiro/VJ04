504c250caecf524dc9bf81f7346ae5c4652e6dd6
Keijiro Takahashi <keijiro@X1-Turbo.local>
2015-01-20 18:47:51 +0900

    Added particle effect to Astrella.

--- Assets/Astrella/Astrella.cs
+++ Assets/Astrella/Astrella.cs
@@ -1,28 +1,59 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Astrella : MonoBehaviour
 {
-    [Range(0, 1)]
-    public float ghostFx = 0;
+    #region Public Settings
 
-    [Range(0, 1)]
-    public float sliceFx = 0;
+    // General settings.
+    public Transform hipNode;
 
-    [Range(0, 1)]
-    public float emission = 0;
+    // Effect parameters.
+    [Range(0, 1)] public float ghostFx = 0;
+    [Range(0, 1)] public float sliceFx = 0;
+    [Range(0, 1)] public float illuminate = 0;
 
-    [Range(0, 1)]
-    public float jitterSpeed = 0.5f;
+    // Animation parameters.
+    [Range(0, 1)] public float jitterSpeed = 0.5f;
 
+    // Skeletal particle settings.
+    public ParticleSystem emitter;
+    [Range(0, 1)] public float particleEmission = 0;
+
+    #endregion
+
+    #region Private Variables
+
+    // Constants.
+    const float emissionPointDensity = 60;
+    const float maxEmissionFrequency = 1000;
+
+    // Renference to renderers.
     Renderer[] renderers;
 
-    void Start()
+    // Particle emision point array.
+    struct EmissionPoint
     {
-        renderers = GetComponentsInChildren<Renderer>();
+        public Transform transform;
+        public Vector3 position;
+        public EmissionPoint(Transform t, Vector3 p)
+        {
+            this.transform = t;
+            this.position = p;
+        }
     }
+    List<EmissionPoint> emissionPoints;
 
-    void Update()
+    // Timer used for particle emission.
+    float particleTimer = 0;
+
+    #endregion
+
+    #region Private Methods
+
+    // Apply the effect settings to the renderers.
+    void ApplyEffectSettings()
     {
         foreach (var r in renderers)
         {
@@ -45,10 +76,83 @@ public class Astrella : MonoBehaviour
                     m.DisableKeyword("FX_GHOST");
                     m.DisableKeyword("FX_SLICE");
                 }
-                m.SetFloat("_Emission", emission * 20);
+                m.SetFloat("_Emission", illuminate * 20);
             }
         }
-
-        JitterJoint.freq = 1.5f * jitterSpeed;
     }
+
+    // Traverse the hierarchy and pick up emission points.
+    void ScanEmissionPointRecursive(Transform node)
+    {
+        foreach (Transform t in node)
+        {
+            if (t.name.EndsWith("Point")) return;
+
+            var intvl = 1.0f / (t.localPosition.magnitude * emissionPointDensity);
+            for (var p = 0.0f; p < 1.0f; p += intvl)
+                emissionPoints.Add(new EmissionPoint(node, t.localPosition * p));
+
+            if (!t.name.EndsWith("Hand")) ScanEmissionPointRecursive(t);
+        }
+    }
+
+    // Emit a given number of particles on randomly chosen emission points.
+    void EmitParticle(int count)
+    {
+        var p = new ParticleSystem.Particle();
+
+        // Shared settings.
+        p.startLifetime = p.lifetime = Random.Range(1.0f, 2.5f);
+        p.angularVelocity = 100;
+        p.color = Color.white;
+
+        for (var i = 0; i < count; i++)
+        {
+            // Pick up an emission point from the array.
+            var ep = emissionPoints[Random.Range(0, emissionPoints.Count)];
+
+            // Set the position.
+            p.position = ep.transform.TransformPoint(ep.position);
+            p.position += Random.insideUnitSphere * (2.0f / emissionPointDensity);
+
+            // Random settings.
+            p.velocity = Random.insideUnitSphere * 0.5f;
+            p.size = Random.Range(0.015f, 0.11f);
+            p.axisOfRotation = Random.onUnitSphere;
+            p.rotation = Random.Range(-180.0f, 180.0f);
+
+            // Emit one particle.
+            emitter.Emit(p);
+        }
+    }
+
+    #endregion
+
+    #region MonoBehaviour
+
+    void Start()
+    {
+        renderers = GetComponentsInChildren<Renderer>();
+
+        // Initialize the emission point list.
+        emissionPoints = new List<EmissionPoint>();
+        ScanEmissionPointRecursive(hipNode);
+    }
+
+    void Update()
+    {
+        // Effects.
+        ApplyEffectSettings();
+
+        // Animation.
+        JitterJoint.freq = 1.5f * jitterSpeed;
+
+        // Particle emission.
+        particleTimer += Time.deltaTime * maxEmissionFrequency * particleEmission;
+        var emission = Mathf.FloorToInt(particleTimer);
+        EmitParticle(emission);
+        particleTimer -= emission;
+    }
+
+    #endregion
 }
