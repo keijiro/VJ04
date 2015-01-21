0f6a2ce5f17480755689dba168bfecb863f81cca
Keijiro Takahashi <keijiro@X1-Turbo.jt.unity3d.com>
2015-01-21 22:35:22 +0900

    Added some more settings.

---
+++ Assets/VJ04/FogBgGear.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FogBgGear : MonoBehaviour
+{
+    public Reaktion.ReaktorLink reaktor;
+    public Gradient gradient;
+
+    void Awake()
+    {
+        reaktor.Initialize(this);
+    }
+
+    void Update()
+    {
+        var color = gradient.Evaluate(reaktor.Output);
+        Camera.main.backgroundColor = color;
+        RenderSettings.fogColor = color;
+    }
+}
