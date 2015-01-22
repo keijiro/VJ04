4fcd916c43ba72e7f3079be9efb38047ef4d466d
Keijiro Takahashi <keijiro@X1-Turbo.local>
2015-01-22 17:07:03 +0900

    Added Camera switcher.

---
+++ Assets/VJ04/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform[] snapPoints;
+    public int selection;
+
+    void Update()
+    {
+        var t = snapPoints[selection];
+
+        var coeff = Mathf.Exp(-2.0f * Time.deltaTime);
+
+        transform.position = Vector3.Lerp(t.position, transform.position, coeff);
+
+        if (transform.rotation != t.rotation)
+            transform.rotation = Quaternion.Slerp(t.rotation, transform.rotation, coeff);
+    }
+
+    public void ChangeTarget(int index)
+    {
+        selection = index;
+    }
+}
